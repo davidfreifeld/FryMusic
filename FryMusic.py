61ffd5392eba39aad75fbb51661b5f2a2a4f0fa7
David Freifeld <david.freifeld@gmail.com>
2015-01-26 03:51:56 +0000

    major refactor of code and adding functionality to retrieve new tracks

--- FryMusic.py
+++ FryMusic.py
@@ -4,22 +4,47 @@
 from getpass import getpass
 from gmusicapi import Mobileclient
 import sqlite3 as lite
+import re
+import glob
 
+def mobileClientLogin(api):
+    """ Logs into the google music account and 
+    returns the username input by the user """
 
-def saveAlbumDatabase():
-    """ Logs into the google music api and 
-    gets all of the songs """
-
-    api = Mobileclient()
-    
     # Log in to google music account
+    usernameEmail = raw_input("Username: ")
     pw = getpass()
-    api.login('fryguy1326@gmail.com', pw)
+    api.login(usernameEmail, pw)
 
-    # Get the list of all song dictionaries
-    library = api.get_all_songs()
+    # Get the username (i.e. drop @gmail.com if it's there)
+    regexUN = re.compile(r"((.*)@gmail.com)|(.*)")
+    regexResult = regexUN.match(usernameEmail)
+    username = usernameEmail
+    if (regexResult.group(2)):
+        username = regexResult.group(2)
+        
+    return username
 
-    # Store the album/artist pairs in a set
+
+def getMaxDBNumber(username):
+    """ Find the current maximum database number in 
+    the directory"""
+    
+    # Find all this user's db files in the directory
+    dbfiles = glob.glob(username+"[.][0-9]*[.]db")
+    regexDBNum = re.compile(username+"[.]([0-9]*)[.]db")
+    maxNumber = 0
+    for dbfile in dbfiles:
+        regResult = regexDBNum.match(dbfile)
+        fileNumber = int(regResult.group(1))
+        if (fileNumber > maxNumber):
+            maxNumber = fileNumber
+    return maxNumber
+
+
+def getAlbumAndTrackSet(library):
+    """ Store the album/artist pairs in a set """
+
     albumList = []
     trackList = []
     for track in library:
@@ -27,9 +52,14 @@ def saveAlbumDatabase():
         trackList.append((track['album'], track['artist'], track['title']))
     albumSet = set(albumList)
     trackSet = set(trackList)
+    return (albumSet, trackSet)
 
-    # Connect to the database and write the album, artist pairs
-    con = lite.connect('frymusic.db')
+
+def saveLibraryToDatabase(dbName, albumSet, trackSet):
+    """ Saves the user's google play songs and albums to 
+    an SQLite database """   
+
+    con = lite.connect(dbName)
 
     with con:
 
@@ -55,14 +85,84 @@ def saveAlbumDatabase():
             cur.execute("INSERT INTO Tracks VALUES(:album, :artist, :title)", 
                 {"album": track[0], "artist": track[1], "title": track[2]})
 
-        # Get the albums from the database and print
-        cur.execute("SELECT rowid, cAlbumTitle, cArtist FROM Albums")
+        return dbName
 
+
+def getAllTracks(dbName):
+    """ Returns a list of tuples of tracks in the db """
+
+    con = lite.connect(dbName)
+
+    with con:
+
+        cur = con.cursor()
+        cur.execute("SELECT cAlbumTitle, cArtist, cTrackTitle FROM Tracks")
         rows = cur.fetchall()
+        return rows
 
-        for row in rows:
-            print "%s) %s | %s" % (row[0], row[1], row[2])
-    
+
+def getAllAlbums(dbName):
+    """ Returns a list of tuples of albums in the db """
+
+    con = lite.connect(dbName)
+
+    with con:
+
+        cur = con.cursor()
+        cur.execute("SELECT rowid, cAlbumTitle, cArtist FROM Albums")
+        rows = cur.fetchall()
+        return rows
+
+
+def getNewTracks(username, currentNumber, previousNumber=-1):
+    """ Returns a set of tracks that exist in the current database
+    but not the previous one """
+
+    if (previousNumber == -1):
+        previousNumber = currentNumber - 1
+
+    currentDBName = username + '.' + str(currentNumber) + '.db'
+    prevDBName = username + '.' + str(previousNumber) + '.db'
+
+    currentTrackSet = set(getAllTracks(currentDBName))
+    prevTrackSet = set(getAllTracks(prevDBName))
+
+    newTrackSet = currentTrackSet - prevTrackSet
+
+    return newTrackSet
+
+
+def printAlbums(dbName):
+    """ Prints a list of all albums in the database """
+
+    rows = getAllAlbums(dbName)
+    for row in rows:
+        print "%s) %s | %s" % (row[0], row[1], row[2])
+
+def printTracksFromSet(trackset):
+    """ Prints a list of the tracks from the given set """
+
+    for track in trackset:
+        print "%s | %s | %s" % (track[0], track[1], track[2])
+   
 
 if __name__ == "__main__":
-    saveAlbumDatabase()    
+    # Get the gmusic client and log in
+    api = Mobileclient()
+    username = mobileClientLogin(api)
+        
+    # Get the list of all song dictionaries
+    library = api.get_all_songs()
+
+    # Get the sets of albums and tracks
+    (albumSet, trackSet) = getAlbumAndTrackSet(library)
+
+    # Find out the number of the most recently saved db
+    maxNumber = getMaxDBNumber(username)
+
+    # Connect to the database and write the album, artist pairs
+    dbName = username + '.' + str(maxNumber+1) + '.db'
+    saveLibraryToDatabase(dbName, albumSet, trackSet)
+
+    # Read the albums from the database and print
+    printAlbums(dbName)
